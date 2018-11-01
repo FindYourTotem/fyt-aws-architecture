606940768d2f971ea527c8ecd9c77665861005a1
Rich Ung <ricky196@yahoo.com>
2018-11-01 15:07:29 -0700

    Updating description

--- tf/lambda_functions.tf
+++ tf/lambda_functions.tf
@@ -5,4 +5,5 @@ resource "aws_lambda_function" "test_lambda" {
   handler          = "lambda_function.lambda_handler"
   source_code_hash = "${base64sha256(file("../lambda_functions/lambda_iot.zip"))}"
   runtime          = "python3.6"
+  description      = "[Terraform] Lambda function for lambda iot"
 }
